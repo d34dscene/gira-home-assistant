a838bb2ce5e98f22a60859294f722cc22e32e8ac
leoyn <leo@mixify.ga>
2022-11-16 21:51:22 +0100

    Allow controlling devices manually via service

--- custom_components/gira/__init__.py
+++ custom_components/gira/__init__.py
@@ -27,7 +27,16 @@ def setup(hass, config):
     except:
         pass
 
+    def handle_set_device_value(call):
+        device_type = call.data.get("device_type")
+        device_id = call.data.get("device_id")
+        value = call.data.get("value")
+
+        ClientSingleton.getInstance().setDeviceValue(device_type, device_id, value)
+        
+
     hass.bus.listen_once(EVENT_HOMEASSISTANT_STOP, onHassStop)
+    hass.services.register(DOMAIN, "set_device_value", handle_set_device_value)
 
     return True
 
@@ -36,6 +45,22 @@ async def async_setup_entry(hass, entry):
     connect(hass, entry.data)
     return True
 
+async def async_unload_entry(hass, entry):
+    client = ClientSingleton.getInstance()
+
+    if client != None:
+        client.close()
+        logger.debug("Closed connection")
+        entityLookupTable = {}
+
+        ok = await PlatformManager.getInstance().unload_platform_integrations(hass, entry)
+
+        if ok == False:
+            return False
+    else:
+        logger.debug("Client is not setup")
+    
+    return True
 
 def connect(hass, config):
     client = ClientSingleton.create(
@@ -46,20 +71,24 @@ def connect(hass, config):
     )
 
     def onDeviceValueListener(_id, value):
-        _id = str(_id)
-        if _id in entityLookupTable:
-            entity = entityLookupTable[_id]
-            entity._value = value
-
-            try:
-                entity.async_write_ha_state()
-            except:
-                pass
+        if client.state == 2:
+            _id = str(_id)
+            if _id in entityLookupTable:
+                entity = entityLookupTable[_id]
+                entity._value = value
+                
+                try:
+                    entity.async_write_ha_state()
+                    logger.debug(f"changed value of entity {entity.name} ({_id}) to {value}")
+                except Exception as exception:
+                    logger.warning(f"failed to change entity value: {exception}")
 
     def onClientReadyListener(token):
         parser = gira_homeserver_api.Parser()
         devices = parser.parse(client.getDevices(), client)
         entities = EntityTranslator.translate_devices_to_entities(devices)
+
+        logger.debug("Connection established")
 
         try:
             with open(os.path.abspath(os.path.dirname(__file__) + "/../gira-custom-sensors.json")) as f:
@@ -77,9 +106,13 @@ def connect(hass, config):
         platformManager.load_platform_integrations(hass, config)
         fillLookupTable(entities)
 
+    def onConnectionErrorListener():
+        logger.warning("Connection error")
+
     client.onDeviceValue(onDeviceValueListener)
     client.onClientReady(onClientReadyListener)
-    client.connect(asynchronous=True, reconnect=True)
+    client.onConnectionError(onConnectionErrorListener)
+    client.connect(asynchronous=True, reconnect=False)
 
 
 def onHassStop(_data):

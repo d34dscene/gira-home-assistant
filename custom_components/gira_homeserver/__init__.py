9d826f9f754ac069f77e9e1e6edb3bff37199dc8
d34dscene <xddd.governeurs@slmail.me>
2025-01-03 15:45:49 +0100

    handle unload better

--- custom_components/gira_homeserver/__init__.py
+++ custom_components/gira_homeserver/__init__.py
@@ -99,10 +99,11 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
-    client = hass.data[DOMAIN].pop(entry.entry_id, None)
+    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
 
-    # Disconnect the client if it was initialized
-    if client:
-        await client.disconnect()
+    if unload_ok:
+        client = hass.data[DOMAIN].pop(entry.entry_id, None)
+        if client:
+            await client.disconnect()
 
-    return True
+    return unload_ok
